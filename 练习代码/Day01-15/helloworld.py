fbab8477d42d4a5c4525b98ad66a5f12383d23d6
AlexGao <gaotianjie2005@qq.com>
2024-03-28 16:39:14 +0800

    add

---
+++ 练习代码/Day01-15/helloworld.py
@@ -0,0 +1,21 @@
+import this
+import turtle
+
+print("hello world")
+
+
+
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
+
